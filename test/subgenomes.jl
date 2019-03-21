c3e3b2e91fddd903649d8a5b1574be2c7e3c7aeb
Arthur ZWAENEPOEL <arzwa@psb.ugent.be>
2019-03-21 15:59:49 +0100

    update dev

--- test/subgenomes.jl
+++ test/subgenomes.jl
@@ -5,7 +5,7 @@ Whale.drawtree(S)
 conf = read_whaleconf("./example/whaleamb.conf")
 slices = get_slices_conf(S, conf["slices"])
 rate_index = Whale.constant_ri(S)
-add_ambiguous!(S, conf)
+#add_ambiguous!(S, conf)
 
 # get some test CCD and see whether the CCD reading works correctly
 ccd = get_ccd("/home/arzwa/coffee/ale-out.1/OG0002088.fasta.nex.treesample.ale", S)[1]
